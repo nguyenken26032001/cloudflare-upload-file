6d9c5282ee83bdcc21d25020c8fdbb1c735f1481
Trần Văn Nguyện <kenn71917@gmail.com>
2024-10-03 00:05:05 +0700

    feature: upload file to cloudflare R2 and get link download

--- src/index.js
+++ src/index.js
@@ -7,9 +7,46 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello from Hono on Cloudflare Workers!'));
+app.get('/hello', (c) => c.text('HELLO WORLD!'));
+
+app.post('/upload', async (c) => {
+	try {
+		const formData = await c.req.formData();
+		const file = formData.get('file');
+
+		if (!file) {
+			return c.text('No file uploaded', 400);
+		}
+
+		const objectKey = normalizeString(file.name); // Sử dụng tên file làm key trong bucket
+		const fileContent = await file.arrayBuffer(); // Đọc nội dung file
+
+		// Upload file lên R2
+		const dataUpload = await c.env.store_file.put(objectKey, fileContent, {
+			httpMetadata: { contentType: file.type }, // Đặt Content-Type của file
+		});
+
+		return c.json({ message: 'File uploaded successfully!', link: `${c.env.URL_DOWNLOAD}/${objectKey}`, dataUpload }, 200);
+	} catch (error) {
+		return c.text(`Error: ${error.message}`, 500);
+	}
+});
+
+function normalizeString(input) {
+	let string = input.toLowerCase();
+	const lastDotIndex = input.lastIndexOf('.');
+	lastDotIndex !== -1 ? (string = string.slice(0, lastDotIndex)) : string;
+	string = string.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	string = string.replace(/\s+/g, '');
+	const timestamp = Date.now();
+	return `${string}-${timestamp}`;
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+	fetch: app.fetch,
 };
